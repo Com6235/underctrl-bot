b3d0c3985096b45d845c6015796548be05333434
notunderctrl <underctrl.io@gmail.com>
2023-03-30 11:39:50 +0300

    Add helperRole in solved command

--- src/commands/admin/solved.js
+++ src/commands/admin/solved.js
@@ -1,6 +1,6 @@
 const { Client, Interaction, PermissionFlagsBits, ChannelType } = require('discord.js');
 
-const { solvedTagId } = require('../../../config.json');
+const { solvedTagId, helperRole } = require('../../../config.json');
 const { checkEmoji } = require('../../../emojis.json');
 
 module.exports = {
@@ -36,10 +36,16 @@ module.exports = {
       threadOwner = starterMessage.author;
     }
 
-    if (interaction.member.permissions.has(PermissionFlagsBits.ManageChannels) || interaction.member.id === threadOwner?.id) {
+    if (
+      interaction.member.permissions.has(PermissionFlagsBits.ManageChannels) ||
+      interaction.member.id === threadOwner?.id ||
+      interaction.member.roles.cache.has((role) => role.id === helperRole)
+    ) {
       try {
         if (interaction.channel.appliedTags?.includes(solvedTagId)) {
-          await interaction.editReply('This thread has been archived again as it had previously been marked as solved.');
+          await interaction.editReply(
+            'This thread has been archived again as it had previously been marked as solved.'
+          );
           await interaction.channel.setArchived(true);
           return;
         }
@@ -47,7 +53,11 @@ module.exports = {
         await interaction.channel.setAppliedTags([...interaction.channel.appliedTags, solvedTagId]);
         const messageAmount = interaction.channel.messageCount;
 
-        await interaction.editReply(`${checkEmoji} ${threadOwner ? `${threadOwner}` : ''} Marked thread as solved after ${messageAmount} messages.`);
+        await interaction.editReply(
+          `${checkEmoji} ${
+            threadOwner ? `${threadOwner}` : ''
+          } Marked thread as solved after ${messageAmount} messages.`
+        );
         await interaction.channel.setArchived(true);
       } catch (error) {
         console.log(`There was an error marking this thread as solved. ${error}`);
